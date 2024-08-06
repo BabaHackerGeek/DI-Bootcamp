f2c7414a735bb280ffb169e7ffb0459cabbd7004
BabaHackerGeek <baroukhabadi@gmail.com>
2024-08-06 14:14:41 +0200

    inital commit

---
+++ Week 1/Day 1/ExerciseXP/ex5.py
@@ -0,0 +1,15 @@
+#🌟 Exercise 5 : Your Information
+#Instructions
+#Create a variable called name, and set it’s value to your name.
+#Create a variable called age, and set it’s value to your age.
+#Create a variable called shoe_size, and set it’s value to your shoe size.
+#Create a variable called info and set it’s value to an interesting sentence about yourself. The sentence must contain all the variables created in parts 1, 2 and 3.
+#Have your code print the info message.
+#Run your code
+
+name = "Baroukh"
+age = 21
+shoe_size = 42
+info = f"Hi there ;) My name is {name} and I'm now {age} years old. My shoe size is {shoe_size} and my actual dream is to help Israel attacking Iran by integrating 8200 unit of Tsahal."
+
+print(info)
---
+++ Week 1/Day 1/ExerciseXP/ex9.py
@@ -0,0 +1,15 @@
+#🌟 Exercise 9 : Tall Enough To Ride A Roller Coaster
+#Instructions
+#Write code that will ask the user for their height in centimeters.
+#If they are over 145cm print a message that states they are tall enough to ride.
+#If they are not tall enough print a message that says they need to grow some more to ride.
+
+user_height = float(input("Please provide your height in centimeters: "))
+
+if user_height > 145:
+    print("Cool! You are tall enough to ride. Enjoy!")
+else:
+    print("Sorry... you need to grow some more to ride :(")
+
+
+
