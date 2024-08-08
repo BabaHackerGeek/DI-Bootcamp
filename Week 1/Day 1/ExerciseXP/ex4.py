6d1493963b28e01cab31cd8db99dfbed4ade4a81
BabaHackerGeek <baroukhabadi@gmail.com>
2024-08-08 22:53:49 +0200

    final versions

--- Week 1/Day 1/ExerciseXP/ex4.py
+++
@@ -1,7 +0,0 @@
-#🌟 Exercise 4 : Your Computer Brand
-#Instructions
-#Create a variable called computer_brand which value is the brand name of your computer.
-#Using the computer_brand variable print a sentence that states the following: "I have a <computer_brand> computer".
-
-computer_brand = "Asus"
-print(f"I have an {computer_brand} computer")
